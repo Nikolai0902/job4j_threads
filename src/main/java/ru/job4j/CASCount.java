a78727944685d8d2a923d0db1efc4c3c392b08e2
Nikolai0902 <kbus94@yandex.ru>
2022-09-12 13:29:11 +0300

    0. CAS - операции

---
+++ src/main/java/ru/job4j/CASCount.java
@@ -0,0 +1,22 @@
+package ru.job4j;
+
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+@ThreadSafe
+
+public class CASCount {
+    private final AtomicReference<Integer> count = new AtomicReference<>();
+
+    public void increment() {
+        int expected;
+        do {
+            expected = count.get();
+        } while (count.compareAndSet(expected, expected + 1));
+    }
+
+    public int get() {
+        return count.get();
+    }
+}
