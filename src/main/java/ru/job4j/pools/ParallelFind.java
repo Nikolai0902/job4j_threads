c13fa235efe4504336764a592f8f01ebb0dc7d41
Nikolai0902 <kbus94@yandex.ru>
2022-09-15 13:38:05 +0300

    3. ForkJoinPool

--- src/main/java/ru/job4j/pools/ParallelFind.java
+++ src/main/java/ru/job4j/pools/ParallelFind.java
@@ -1,6 +1,5 @@
 package ru.job4j.pools;
 
-import java.util.List;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
 
@@ -8,50 +7,51 @@ import java.util.concurrent.RecursiveTask;
  * Параллельный поиск индекса в массиве объектов с применением ForkJoinPool.
  * В целях оптимизации, если размер массива не больше 10, используется обычный линейный поиск.
  */
-public class ParallelFind extends RecursiveTask<Integer> {
+public class ParallelFind<T> extends RecursiveTask<Integer> {
 
     private final static int SIZE_LINE_SEARCH = 10;
-    private final int value;
-    private final Integer[] array;
+    private final T value;
+    private final T[] array;
     private final int from;
     private final int to;
 
-    public ParallelFind(int value, Integer[] array, int from, int to) {
+    public ParallelFind(T value, T[] array, int from, int to) {
         this.value = value;
         this.array = array;
+        valid(value, array);
         this.from = from;
         this.to = to;
     }
 
+    private void valid(T value, T[] array) {
+        if (value.getClass() != array[0].getClass()) {
+            throw new IllegalArgumentException("Разные типы данных.");
+        }
+    }
+
     @Override
     protected Integer compute() {
-        if (from == to) {
-            return array[to] == value ? to : -1;
+        if (to - from <= SIZE_LINE_SEARCH) {
+            for (int i = from; i <= to; i++) {
+               if (array[i] == value) {
+                   return i;
+               }
+            }
+            return null;
         }
         int mid = (from + to) / 2;
-        ParallelFind left = new ParallelFind(value, array, from, mid);
-        ParallelFind right = new ParallelFind(value, array, mid + 1, to);
+        ParallelFind<T> left = new ParallelFind<T>(value, array, from, mid);
+        ParallelFind<T> right = new ParallelFind<T>(value, array, mid + 1, to);
         left.fork();
         right.fork();
-        int leftIndex = left.join();
-        int rightIndex = right.join();
-        return leftIndex == -1 ? rightIndex : leftIndex;
+        Integer leftIndex = left.join();
+        Integer rightIndex = right.join();
+        return leftIndex == null ? rightIndex : leftIndex;
     }
 
-    private static Integer lineSearch(int value, Integer[] array) {
-        return List.of(array).indexOf(value);
-    }
-
-    public static int startSearch(int value, Integer[] array) {
-        if (array.length <= SIZE_LINE_SEARCH) {
-            return lineSearch(value, array);
-        }
+    public static <T> Integer startSearch(T value, T[] array) {
         ForkJoinPool forkJoinPool = new ForkJoinPool();
-        return forkJoinPool.invoke(new ParallelFind(value, array, 0, array.length - 1));
-    }
-
-    public static void main(String[] args) {
-        Integer[] values = new Integer[]{1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1};
-        System.out.println(ParallelFind.startSearch(0, values));
+        return forkJoinPool.invoke(new ParallelFind<>(value, array, 0, array.length - 1));
     }
 }
+
---
+++ src/test/java/ru/job4j/pools/ParallelFindTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.pools;
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.cache.OptimisticException;
+
+import static org.assertj.core.api.Assertions.*;
+
+class ParallelFindTest {
+
+    @Test
+    void testSearchException() {
+        Integer[] values = new Integer[]{0, 1, 100, 3, 4};
+        assertThatThrownBy(() -> ParallelFind.startSearch("100", values))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void testSearchSizeArrayLess10() {
+        Integer[] values = new Integer[]{0, 1, 100, 3, 4};
+        var result = ParallelFind.startSearch(100, values);
+        assertThat(result).isEqualTo(2);
+    }
+
+    @Test
+    void testSearchSizeArrayMore10() {
+        Integer[] values = new Integer[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 100, 12, 13};
+        var result = ParallelFind.startSearch(100, values);
+        assertThat(result).isEqualTo(11);
+    }
+
+    @Test
+    void testSearchSize0() {
+        Integer[] values = new Integer[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 100, 12, 13};
+        var result = ParallelFind.startSearch(1000, values);
+        assertThat(result).isNull();
+    }
+}
